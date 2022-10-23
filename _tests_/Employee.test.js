ab01c2664eaf79315d97193734ba2da2a320d56b
michael seaman <mseaman26@gmail.com>
2022-10-22 21:41:02 -0700

    got folder structure, installed inquirer and jest, started building test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require ("../lib/Employee")
+
+describe("Employee", () => {
+    describe("initialization", () => {
+        it("should include three properties: name, id, and email", () => {
+
+        })
+    })
+})
